60201a429afc047388bc3fd089c6d4c50f9eb6fc
Marcus <Marcus Pratley>
2020-02-20 20:36:30 +0000

    a poor mans' perceptron, plus project scaffolding

---
+++ Perceptron.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class Perceptron(object):
+
+    def __init__(self, no_of_inputs):
+        self.weights = np.zeros(no_of_inputs + 1)
+
+    def predict(self, inputs):
+        summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        if summation > 0:
+            activation = 1
+        else:
+            activation = 0
+        return activation
