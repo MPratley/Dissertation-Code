1512a8d56ce207012d8d9529dc00efaeef68be8c
Marcus <Marcus Pratley>
2020-02-20 21:15:30 +0000

    better name

--- Perceptron.py
+++ Perceptron.py
@@ -6,7 +6,7 @@ class Perceptron(object):
     def __init__(self, no_of_inputs):
         self.weights = np.zeros(no_of_inputs + 1)
 
-    def predict(self, inputs):
+    def output(self, inputs):
         summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
         if summation > 0:
             activation = 1
