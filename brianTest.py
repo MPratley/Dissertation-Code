b5c1d16d0c748e58e451bbe70bdfb34168437f21
Marcus <Marcus Pratley>
2020-03-04 21:21:19 +0000

    LIF stuff

--- brianTest.py
+++ brianTest.py
@@ -10,11 +10,13 @@ tau : second
 
 G = NeuronGroup(3, eqs, threshold='v>1', reset='v = 0', method='exact')
 G.I = [2, 2, 0]
-G.tau = [10, 100]*ms
+G.tau = [10, 5, 100]*ms
 
 # Comment these two lines out to see what happens without Synapses
 S = Synapses(G, G, on_pre='v_post += 0.2')
-S.connect(i=0, j=1)
+S.connect(i=0, j=2)
+S.connect(i=1, j=2)
+
 
 M = StateMonitor(G, 'v', record=True)
 
@@ -22,6 +24,7 @@ run(100*ms)
 
 plt.plot(M.t/ms, M.v[0], label='Neuron 0')
 plt.plot(M.t/ms, M.v[1], label='Neuron 1')
+plt.plot(M.t/ms, M.v[2], label='Neuron 2')
 plt.xlabel('Time (ms)')
 plt.ylabel('v')
 plt.legend()
