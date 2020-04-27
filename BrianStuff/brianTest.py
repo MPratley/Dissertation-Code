e3dd5bf996ba6363a21535abe5d56acc6ea80495
Marcus Pratley <marcus@Marcus-Macbook-Air.local>
2020-04-27 15:36:08 +0100

    things

--- BrianStuff/brianTest.py
+++ BrianStuff/brianTest.py
@@ -1,6 +1,7 @@
 from brian2.units import ms
 from brian2 import NeuronGroup, Synapses, StateMonitor, run
 import matplotlib.pyplot as plt
+import numpy as np
 
 eqs = '''
 dv/dt = (I-v)/tau : 1
@@ -8,24 +9,65 @@ I : 1
 tau : second
 '''
 
-G = NeuronGroup(3, eqs, threshold='v>1', reset='v = 0', method='exact')
-G.I = [2, 2, 0]
-G.tau = [10, 5, 100]*ms
+# Predefined seed so the experiment is repeatable.
+np.random.seed(67726)
+
+DrivingGroup = NeuronGroup(9, eqs, threshold='v>1', reset='v = 0', method='exact')
+DrivingGroup.I = np.random.uniform(low=0, high=2, size=9)
+DrivingGroup.tau = np.full(
+    shape=9,
+    fill_value=10,
+    dtype=np.int)*ms
+
+# DrivingGroup.tau = [10, 5, 100]*ms
+
+OutputGroup = NeuronGroup(1, eqs, threshold='v>2', reset='v = 0', method='exact')
+OutputGroup.I = [0]
+OutputGroup.tau = [100]*ms
 
 # Comment these two lines out to see what happens without Synapses
-S = Synapses(G, G, on_pre='v_post += 0.2')
-S.connect(i=0, j=2)
-S.connect(i=1, j=2)
+S = Synapses(DrivingGroup, OutputGroup, on_pre='v_post += 0.2')
+S.connect(i=np.arange(9), j=0)
+# S.connect(i=0, j=2)
+# S.connect(i=1, j=2)
+
+DrivingMonitor = StateMonitor(DrivingGroup, 'v', record=True)
+OutputMonitor = StateMonitor(OutputGroup, 'v', record=True)
+
+def visualise_connectivity(S):
+    Ns = len(S.source)
+    Nt = len(S.target)
+    plt.figure(figsize=(3, 6))
+    # plt.subplot(121)
+    plt.plot(np.zeros(Ns), np.arange(Ns), 'ok', ms=10)
+    plt.plot(np.ones(Nt), np.arange(Nt), 'ok', ms=10)
+    for i, j in zip(S.i, S.j):
+        plt.plot([0, 1], [i, j], '-k')
+    # plt.xticks([0, 1], ['Source', 'Target'])
+    # plt.ylabel('Neuron no.')
+    plt.xlim(-0.1, 1.1)
+    plt.ylim(-1, max(Ns, Nt))
+    # plt.subplot(122)
+    # plt.plot(S.i, S.j, 'ok')
+    # plt.xlim(-1, Ns)
+    # plt.ylim(-1, Nt)
+    # plt.xlabel('Source neuron index')
+    # plt.ylabel('Target neuron index')
+
+visualise_connectivity(S)
+plt.axis('off')
+plt.savefig('2.png', transparent=True)
 
 
-M = StateMonitor(G, 'v', record=True)
+# run(100*ms)
 
-run(100*ms)
-
-plt.plot(M.t/ms, M.v[0], label='Neuron 0')
-plt.plot(M.t/ms, M.v[1], label='Neuron 1')
-plt.plot(M.t/ms, M.v[2], label='Neuron 2')
-plt.xlabel('Time (ms)')
-plt.ylabel('v')
-plt.legend()
-plt.show()
+# plt.figure()
+# plt.plot(DrivingMonitor.t/ms, DrivingMonitor.v[0], label='Input Neuron 1 of 9')
+# plt.plot(OutputMonitor.t/ms, OutputMonitor.v[0], label='Output Neuron')
+# # plt.plot(M.t/ms, M.v[1], label='Neuron 1')
+# # plt.plot(M.t/ms, M.v[2], label='Neuron 2')
+# plt.xlabel('Time (ms)')
+# plt.ylabel('v')
+# plt.legend()
+# plt.show()
+# plt.savefig('1.png')
--- src/IF.py
+++ src/IF.py
@@ -1,26 +1,47 @@
-import numpy as np 
+import numpy as np
 from scipy.integrate import solve_ivp
+import matplotlib
+import matplotlib.pyplot as plt
 
-
-# Parameters
+# Arbitrary Parameters
 Tmax = 100  # ms - Time to Run
 dt = 0.1    # ms - Time Step
-numSteps = int(Tmax/dt)
-Vmax = 50   # mV - Threshold Voltage
-R = 5       # Ohms Resistance
-C = 2       # mF - Capacitance
-I = 1.5     # mA - Input Current
+Vrest = 0   # mV - Resting Voltage
+Vmax = 10   # mV - Threshold Voltage
+R = 6       # Ohms Resistance
+C = 3       # mF - Capacitance
+I = 2       # mA - Input Current
 tau = R*C   # ms - Time Constant (7.5)
 
+# Differential 
 def dv_dt(t, v):
-    if v >= Vmax:
-        return -Vmax
-    return (-v + R*I)/tau
+    return (-v + Vrest + R*I)/tau
 
-t_arr = np.linspace(0,Tmax,numSteps)
-v0 = [0]
-sol = solve_ivp(dv_dt, [0,Tmax], v0, t_eval=t_arr)
+t_arr = np.arange(0, Tmax, dt)
+s_arr = np.zeros(len(t_arr))
+v_arr = np.zeros(len(t_arr))
+v_arr[-1] = Vrest
+
+for i, t in enumerate(t_arr):
+    Vm = v_arr[i-1]
+    if Vm >= Vmax:
+        Vm = Vrest
+        s_arr[i] = 1
+    else:
+        Vm = Vm + dt*dv_dt(t, Vm)
+    v_arr[i] = Vm
+
+fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+
+ax1.plot(t_arr, s_arr, marker='.', markersize=20, linestyle='none')
+ax1.set(ylabel='Spikes')
+ax1.axis([0,Tmax, 0.5, 1.5])
+
+ax2.plot(t_arr, v_arr)
+ax2.set(xlabel='time (ms)', ylabel='voltage (mV)')
+ax2.grid()
 
 
-
+fig.savefig("singleSpikingNeuron.png")
+plt.show()
 
